e158938f755410ea303bb490f27f2dcbad4bed0f
Vitalii Sviderskyi <vsviderskyi@altinity.com>
2021-05-11 14:48:12 -0400

    bccard

---
+++ tests/bccard.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+#  Add user agent
+# options.add_argument('user-agent=')
+driver = webdriver.Chrome(chrome_options=options, executable_path='/snap/bin/chromium.chromedriver')
+driver.get('https://www.bccard.com/app/merchant/Login.do')
+driver.implicitly_wait(10)
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.alert_is_present())
+alert = driver.switch_to.alert
+assert "INISAFE CrossWebEX" in alert.text
+alert.dismiss()
+
+driver.find_element_by_css_selector('li>#userid').send_keys('id')
+field = driver.find_element_by_css_selector('span>#passwd')
+driver.execute_script(f"arguments[0].value='pw'", field)
+
+
+time.sleep(10)
